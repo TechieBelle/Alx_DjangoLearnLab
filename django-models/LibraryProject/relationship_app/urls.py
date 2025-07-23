7bea7d59cabbbf452a1c39e2edf76f3279ed75d3
Folashade Bello <folashadebello.b@gmail.com>
2025-07-23 16:46:02 +0100

    task 4 urls.py corrected

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('member/', views.member_view, name='member_view'),
 
     # Task 4: CRUD operations for Book model
-     path('book/add/', views.add_book, name='add_book'),
-    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
+     path('add_book', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('book_delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
