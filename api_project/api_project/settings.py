0b32be5faf4356de15ec568801e3ced04695d6e7
Folashade Bello <folashadebello.b@gmail.com>
2025-08-01 01:32:19 +0100

    files restructured

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-es=#2do#kym6=_6ui6%y_m1y&11n9!qtdj=!^w&#h5-e!padqj'
+SECRET_KEY = 'django-insecure-cpebz##)gvn$spd=pulanw!=k(b-dw&5hs39r_yeul7!of$kq*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'api.apps.ApiConfig',  
 ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
