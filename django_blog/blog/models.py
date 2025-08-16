e5e31f5ca30feded87d8556bf55e8e79364259c3
Folashade Bello <folashadebello.b@gmail.com>
2025-08-16 04:15:37 +0100

    added a User model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.username
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
