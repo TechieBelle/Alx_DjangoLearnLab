9bb74bbd6835a186f476b28ad85e593c236aa7da
Folashade Bello <folashadebello.b@gmail.com>
2025-08-22 11:20:55 +0100

    Task 1 added login/ to blog.urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path, include
 from .import views
 from .views import Index, RegisterView, profile_view
-
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
      
     path('register/', RegisterView.as_view(), name='register'),
     path("profile/", views.profile_view, name="profile"),
     
+        # Auth views (login/logout) using Django built-ins
+    path("login/",  auth_views.LoginView.as_view(template_name="accounts/registration/login.html"), name="login"),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="accounts/logout.html"),
+        name="logout",
+    ),
+
 ]
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,21 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.views import LogoutView
 from django.urls import path,include
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    # Registration & profile from our app
-    path("", include("blog.urls")),   # register, profile
+  p
+    path("", include("blog.urls")),  
     path('admin/', admin.site.urls),
-    # path('', include('blog.urls')),  # Include the blog app's URLs
-    
-    # Auth views (login/logout) using Django built-ins
-    path("login/",  auth_views.LoginView.as_view(template_name="accounts/registration/login.html"), name="login"),
-    path(
-        "logout/",
-        LogoutView.as_view(template_name="accounts/logout.html"),
-        name="logout",
-    ),
-
+     
     
 ]
